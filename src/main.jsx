e9f7db148275db17a51e272d0f0b9a6afaf0c66a
iamargentum <rajat.rj97@gmail.com>
2023-01-07 14:03:48 +0530

    Remove auth-related endpoints

--- src/main.jsx
+++ src/main.jsx
@@ -2,25 +2,11 @@ import './index.css'
 import React from 'react'
 import { MathApp } from './routes/app'
 import ReactDOM from 'react-dom/client'
-import { LoginView } from './routes/login'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { RegisterView } from './routes/register'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <h1>hi! nothing much to see here :)</h1>
-  },
-  {
-    path: "/register",
-    element: <RegisterView />
-  },
-  {
-    path: "/login",
-    element: <LoginView />
-  },
-  {
-    path: "/app",
     element: <MathApp />
   }
 ])
